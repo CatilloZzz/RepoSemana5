7cf14417713306f0d899d3bb7671b9cbf1d55ccd
CatilloZzz <jireyes@uamv.edu.ni>
2026-09-16 14:25:32 -0600

    feat: implementar clase Client y utilidad DataStore

--- src/main/java/ni/edu/uam/clientdesk/models/Client.java
+++ src/main/java/ni/edu/uam/clientdesk/models/Client.java
@@ -1,4 +1,35 @@
 package ni.edu.uam.clientdesk.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Client {
+
+    private String nombres;
+    private String apellidos;
+    private String tipoCliente;
+    private String ciudad;
+    private LocalDate fechaNacimiento;
+    private String tipoSolicitud;
+    private List<String> servicios;
+    private String fotoPath; // guardamos la RUTA de la foto, no la imagen en sí
+
+    public String getNombreCompleto() {
+        return nombres + " " + apellidos;
+    }
+
+    public String getServiciosTexto() {
+        if (servicios == null || servicios.isEmpty()) {
+            return "Ninguno";
+        }
+        return String.join(", ", servicios);
+    }
 }
--- src/main/java/ni/edu/uam/clientdesk/utils/DataStore.java
+++ src/main/java/ni/edu/uam/clientdesk/utils/DataStore.java
@@ -1,4 +1,29 @@
 package ni.edu.uam.clientdesk.utils;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import lombok.Getter;
+import lombok.Setter;
+import ni.edu.uam.clientdesk.models.Client;
+
 public class DataStore {
+
+    private static DataStore instance;
+
+    @Getter
+    private final ObservableList<Client> clients = FXCollections.observableArrayList();
+
+    @Getter
+    @Setter
+    private String currentUser;
+
+    private DataStore() {
+    }
+
+    public static DataStore getInstance() {
+        if (instance == null) {
+            instance = new DataStore();
+        }
+        return instance;
+    }
 }
