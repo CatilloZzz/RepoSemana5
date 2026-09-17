036211188ba81a51b830aebf32f40edf6fefe4c8
BJavi2106 <bjrochav@uamv.edu.ni>
2026-09-17 00:38:12 -0600

    feat: finalizar interfaz y configuracion de inicio

--- src/main/java/ni/edu/uam/clientdesk/Launcher.java
+++ src/main/java/ni/edu/uam/clientdesk/Launcher.java
@@ -1,9 +1,30 @@
 package ni.edu.uam.clientdesk;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Launcher {
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                Launcher.class.getResource(
+                        "/ni/edu/uam/clientdesk/fxml/Login.fxml"
+                )
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("ClientDesk - Inicio de sesión");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        Application.launch( args);
+        launch(args);
     }
 }
